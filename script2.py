ea241e66c5a4cb5fa05d479d2037c3c21b883813
NishantK01 <nishantkumar.khatri@flemingcollege.ca>
2024-01-18 01:01:32 -0500

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,5 @@
 #print lines from Zen of Python
 print("Flat is better than nested.")
 print("Sparse is better than dense.")
+print("Readability counts.")
+print("Errors should never pass silently")
